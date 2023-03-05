f0b0e1e89b3482279c5f50cf2e4f6702ba0dad37
Shamilx <shamil.shamiyevx@gmail.com>
2023-03-05 14:11:15 +0400

    Demo Version

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+const wrapper = document.getElementById('wrapper');
+const calculatorInput = document.getElementById('calculator-input').children[0];
+var problem = "";
+
+function Solve(problem) {
+
+    // Remove duplicate  +  ;  -  ;  /  ;  * chars
+
+    for (let i = 0; i < problem.length; i++) {
+        if (problem[i] == problem[i + 1] && (problem[i] == "+" || problem[i] == "-"
+            || problem[i] == "*" || problem[i] == "/")) {
+            problem = problem.substring(0, i) + problem.substring(i + 1, problem.length);
+            i = i - 1;
+        }
+    }
+
+    // Check if there is + - / * in the end of string
+
+    console.log(problem);
+    return problem;
+}
+
+wrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+
+    if (!isButton) {
+        return;
+    }
+
+    const pressedButton = event.target.textContent;
+
+    if (pressedButton == "AC") {
+        problem = "";
+        calculatorInput.textContent = "0";
+        return;
+    }
+
+    if (calculatorInput.textContent.length > 9) {
+        return;
+    }
+
+    if (pressedButton == "+" || pressedButton == "-"
+        || pressedButton == "*" || pressedButton == "/") {
+
+        problem += calculatorInput.textContent;
+        calculatorInput.textContent = pressedButton;
+        return;
+    }
+
+
+
+    if (pressedButton == "=") {
+        problem += calculatorInput.textContent;
+
+        var answer = Solve(problem);
+        calculatorInput.textContent = answer;
+        problem = answer;
+        return;
+    }
+
+
+    if (calculatorInput.textContent == "0")
+        calculatorInput.textContent = pressedButton;
+    else if (calculatorInput.textContent.length < 9)
+        calculatorInput.textContent += pressedButton
+
+})
